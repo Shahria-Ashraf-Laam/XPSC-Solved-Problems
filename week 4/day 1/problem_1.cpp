2b72a5ad3bca31be0a43acecd45f7e7ded1e1bed
Shahria-Ashraf-Laam <shahria.ashraf45@gmail.com>
2024-11-19 23:52:54 +0600

    Add files via upload
    week-4, day-1 codes added

---
+++ week 4/day 1/problem_1.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    long long k; 
+    cin>>n>>k;
+    vector<int> v(n);
+    for( int i=0; i<n; i++ ){
+        cin>>v[i];
+    }
+
+    int l=0,r=0,ans=0;
+    long long sum=0;
+    while(r<n)
+    {
+        sum += v[r];
+        if( sum <= k )
+        {
+            ans = max(ans, r-l+1);
+        }
+        else
+        {
+            sum -= v[l];
+            l++;
+        }
+        r++;
+    }
+
+    cout<< ans <<"\n";
+
+  return 0;
+}
+
+
+/*
+problem link- https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/A
+problem sheet: week-4, Topicwise-1(Segment with Small Sum)
+*/
