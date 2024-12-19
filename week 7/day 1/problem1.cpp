5e4e73275575c3bcb371384d505b2f3f5c41d9a1
Shahria-Ashraf-Laam <shahria.ashraf45@gmail.com>
2024-12-19 23:33:44 +0600

    Add files via upload
    week-7, day-1 codes added

---
+++ week 7/day 1/problem1.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin>>n;
+    vector<int> v(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i];
+    }
+    
+    bool ok = false;
+    for(int mask=0; mask<(1<<n); mask++)
+    {
+        int sum=0;
+        for(int k=0; k<n; k++)
+        {
+            if( (mask>>k) & 1 )
+            {
+                sum+=v[k];
+            }
+            else
+            {
+                sum-=v[k];
+            }
+        }
+
+        if(sum%360==0)
+        {
+            ok = true;
+            break;
+        }
+    }
+
+    if(ok) cout<<"YES\n";
+    else cout<<"NO\n";
+
+    return 0;
+}
+
+
+
+/*
+link- https://codeforces.com/problemset/problem/1097/B
+week-7, topicwise-1
+*/
