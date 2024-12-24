e230d776f44f6cbc5a12124d5e106407e843f60b
Shahria-Ashraf-Laam <shahria.ashraf45@gmail.com>
2024-12-24 23:43:16 +0600

    Add files via upload

---
+++ week 8/day 1 (no problems solved)/prac.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin>>t;
+    while(t--)
+    {
+    }
+
+    return 0;
+}
+/*
+problem link: https://codeforces.com/problemset/problem/1669/H
+week-7, topicwise-3
+*/
