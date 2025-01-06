fa35e537a01c9337981493281991fb0b5e4ddcba
Shahria-Ashraf-Laam <shahria.ashraf45@gmail.com>
2025-01-06 23:49:23 +0600

    Add files via upload
    week-10, day-5 codes added

---
+++ week 10/day 5/prac.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    
+
+    return 0;
+}
+
+
+
+
+/*
+problem link: https://codeforces.com/problemset/problem/1977/A
+details: week-10, 
+*/
