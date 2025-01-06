e279447b09c708247b12ded84fbc135c32b7ec34
Shahria-Ashraf-Laam <shahria.ashraf45@gmail.com>
2025-01-07 00:46:15 +0600

    Add files via upload

---
+++ week 10/day 5/problem2.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    ll n,x,sum=0,ans=0;
+    cin>>n>>x;
+
+    map<ll,ll> mp;
+    mp[0]=1;
+
+    vector<int> v(n);
+    for(int i=0; i<n; i++){
+        cin>>v[i];
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        sum += v[i];
+        ans += mp[sum-x];
+        mp[sum]++;
+    }
+    cout<< ans <<'\n';
+
+    return 0;
+}
+
+
+
+
+/*
+problem link: https://cses.fi/problemset/task/1661
+details: week-10, random-4 (subarray sums 2)
+*/
