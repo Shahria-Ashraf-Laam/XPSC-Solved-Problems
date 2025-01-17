794aa688498019ada516f58d29525a65e2d3861e
Shahria-Ashraf-Laam <shahria.ashraf45@gmail.com>
2025-01-17 23:50:04 +0600

    Add files via upload
    week-11, day-6 code added

---
+++ week 11/day 6/prac.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    
+
+
+    return 0;
+}
+
+
+/*
+link: https://codeforces.com/problemset/problem/1957/A
+details: 
+*/
